88af990978815836cd4ae2addf03848abdcea6fc
Keisha Perry <keisha@hey.com>
2021-05-29 11:10:52 -0500

    setup config and env vars, add sample env file

--- config/config.js
+++ config/config.js
@@ -1,18 +1,13 @@
-require("dotenv").config();
-
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
-  Development: {
-    username: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: process.env.DATABASE,
-    host: process.env.DBHOST,
-    dialect: "mysql"
-  },
-  Production: {
-    username: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: process.env.DATABASE,
-    host: process.env.DBHOST,
-    dialect: "mysql"
-  }
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
 };
+module.exports.database = {
+  username: process.env.DBUSER,
+  password: process.env.DBPASS,
+  name: process.env.DATABASE,
+  host: process.env.DBHOST,
+  port: process.env.DBPORT,
+}
